be08bd138f9acebfb9d1aa0422c878096c816a52
ilah21 <53281881+ilah21@users.noreply.github.com>
2022-02-16 09:59:24 +0800

    finished

---
+++ config/routes.rb
@@ -0,0 +1,24 @@
+Rails.application.routes.draw do
+  resources :friends 
+    
+
+  devise_for :users, controllers: { registrations: "registrations" }
+  
+  # get 'home/index'
+  get 'home/about'
+  #root 'home#index'
+  root 'friends#index'
+  
+  devise_scope :user do
+    get 'users/sign_up' => "registrations#new"
+    get 'users/sign_in' => "devise/sessions#new"
+    get 'users/sign_out' => "devise/sessions#destroy"
+  end
+
+  
+  
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
